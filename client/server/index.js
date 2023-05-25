31d5ef7e316da73821d3bb74d6f62897ff6cea48
Cherit <cherit@Cherits-MacBook-Air.local>
2023-05-25 19:43:57 +0530

    removed

--- client/server/index.js
+++
@@ -1,8 +0,0 @@
-const express=require("express")
-const dotenv=require("dotenv")
-const app=express();
-dotenv.config()
-
-app.listen(process.env.PORT_NUMBER,()=>{
-    console.log(`BE Server is running on PORT ${process.env.PORT_NUMBER}`)
-})
