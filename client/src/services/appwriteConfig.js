fe1acb503b24c64521c265d385ce69b00605fa55
pankajchanana <hellopankajchanana@gmail.com>
2025-01-30 05:00:49 +0530

    Update appwriteConfig.js

--- client/src/services/appwriteConfig.js
+++ client/src/services/appwriteConfig.js
@@ -2,10 +2,10 @@ import {Account,Client,Databases,Storage} from 'appwrite'
 
 const client = new Client();
 
-client.setEndpoint("https://cloud.appwrite.io/v1").setProject("646f96814ce0bd792d15");
+client.setEndpoint("https://cloud.appwrite.io/v1").setProject("6799cfa2002acf1ad11a");
 
 export const account = new Account(client)
 
-export const databases = new Databases(client,"646f96a60d5767f59620")
+export const databases = new Databases(client,"6799d028002bbed3a90f")
 
 export const  storage = new Storage(client)
